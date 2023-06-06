43293fa845ac3a6d810adcc7288c55a0fff2e0d3
ZCP1 <zcp1@ZCP1s-MacBook-Pro.local>
2023-06-06 11:47:44 -0400

    Appending a html element to another

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-    console.log('I am working')
+    //Grabs the element with ID starting-point
+    const startingElement = $("#starting-point");
+    //Creates a new div
+    const siblingElement = $("<div></div>");
+    //Add text to the new div
+    siblingElement.text("This is the next part");
+    //Appends the new div to startingElement
+    startingElement.after(siblingElement);
+
+    const favoriteFoods = $("#favorite-foods");
+    console.log((favoriteFoods[0].innerHTML))
+    const favFoods = ["pizza", "falafel", "bolani", "hot dog"];
+
+    favFoods.forEach(food => {
+        favoriteFoods.after($("<div></div>").text(food));
+    })
 })
